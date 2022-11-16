69b000b401c909140aeab8bf288047c08676e519
joao_castro <jtcastro0@gmail.com>
2022-11-16 12:18:32 +0000

    New function: changePassword

--- src/Database.java
+++ src/Database.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Database {
     public Connection c;
@@ -38,9 +39,9 @@ public class Database {
         System.out.println("Entry inserted successfully.");
     }
 
-    public void delID(int ID){
+    public void adminDelID(int ID){
         /*
-         * NAME: delID
+         * NAME: adminDelID
          * RETURNS: none
          * PARAMETERS:
          * * ID - ID from client to delete (deletes all data in login table)
@@ -67,7 +68,7 @@ public class Database {
 
     public boolean checkLogin(String email, String posPassword, String phonen){
         /*
-         * NAME: checkPassword
+         * NAME: checkLogin
          * RETURNS: none
          * PARAMETERS:
          * * email - user email, must be verified (it cannot exist already)
@@ -116,5 +117,65 @@ public class Database {
         }
 
         return false; // if function reaches this point, then login was not successful
+    }
+
+    public boolean changePassword(String email, String posPassword, String phonen)
+    {
+        /*
+         * NAME: changePassword
+         * RETURNS: none
+         * PARAMETERS:
+         * * email - user email, must be verified (it cannot exist already)
+         * * posPassword - user password, must be compared with password in database
+         * * phonen - user phone number, can also be used to login
+         */
+
+        Scanner myObj = new Scanner(System.in);
+        String findHashed;
+
+        try {
+            // Open connection to database loseit.login
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://db.fe.up.pt:5432/pswa0506",
+                            "pswa0506", "XPdYrXJb");
+
+            // Search email in database
+            stmt = c.createStatement();
+            rs = stmt.executeQuery("SELECT password, id from loseit.login where email = '"+ email +"' OR phonen = '"+ phonen +"';");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String realPassword = rs.getString("password");
+
+                findHashed = Integer.toString(posPassword.hashCode());
+
+                if(Objects.equals(realPassword, findHashed)){
+                    System.out.println("Introduza a nova password");
+                    String newPass = Integer.toString(myObj.nextLine().hashCode());
+
+                    String sql = "UPDATE loseit.login SET password ='"+newPass+"' where email = '"+email+"';";
+                    stmt.executeUpdate(sql);
+
+                    rs.close();
+                    stmt.close();
+                    c.close();
+                    return true;
+                }
+                else{
+                    System.out.println("Invalid password.");
+                }
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+            System.out.println();
+        }
+        return false;
     }
 }
