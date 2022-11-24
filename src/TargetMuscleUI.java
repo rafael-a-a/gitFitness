8fac452b3a6e11ca2d6dc2f8d02f232ae2189b8c
ricardo <up201906663@edu.fe.up.pt>
2022-11-24 22:42:35 +0000

    Updated TargetMuscleUI

--- src/TargetMuscleUI.java
+++ src/TargetMuscleUI.java
@@ -5,40 +5,33 @@ import java.awt.event.ActionListener;
 public class TargetMuscleUI {
 
     private JPanel targetPanel;
-    private JButton button1;
+    private JButton confirmButton;
     private JPanel panel1;
-    private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JCheckBox checkBox3;
-    private JCheckBox checkBox4;
-    private JCheckBox checkBox5;
-    private JCheckBox checkBox6;
-    private JCheckBox checkBox7;
-    private JCheckBox checkBox8;
-    private JCheckBox checkBox9;
-    private JCheckBox checkBox10;
+    private JCheckBox absCheck;
+    private JCheckBox backCheckBox;
+    private JCheckBox chestCheckBox;
+    private JCheckBox upperLegsCheckBox;
+    private JCheckBox lowerLegsCheckBox;
+    private JCheckBox shouldersCheckBox;
+    private JCheckBox bicepsCheckBox;
+    private JCheckBox cardioCheckBox;
+    private JCheckBox glutesCheckBox;
+    private JCheckBox tricepsCheckBox;
     private JButton backButton;
 
     private final User currentUser;
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-
-    }
 
     TargetMuscleUI(User u){
 
         currentUser = u;
 
-        int windowHeight = 300;
-        int windowWidth = 300;
-        int buttonHeight = 30;
-        int buttonWidth = 160;
-
+        int windowHeight = 500;
+        int windowWidth = 500;
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(windowWidth, windowHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(true);
+        frame.setResizable(false);
         frame.setVisible(true);
         frame.add(targetPanel);
         backButton.addActionListener(new ActionListener() {
@@ -46,9 +39,49 @@ public class TargetMuscleUI {
             public void actionPerformed(ActionEvent e) {
                 if(e.getSource() == backButton) {
                     frame.dispose();
-                    WorkoutUI myTarget = new WorkoutUI(currentUser);
+                    WorkoutUI myWorkout = new WorkoutUI(currentUser);
                 }
             }
         });
+        /*ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==absCheck){
+
+                    
+                } else if (e.getSource()==backCheckBox) {
+                    
+                } else if (e.getSource()==bicepsCheckBox) {
+                    
+                } else if (e.getSource()==cardioCheckBox) {
+                    
+                } else if (e.getSource()==chestCheckBox) {
+                    
+                } else if (e.getSource()==glutesCheckBox) {
+                    
+                } else if (e.getSource()==lowerLegsCheckBox) {
+                    
+                } else if (e.getSource()==shouldersCheckBox) {
+                    
+                } else if (e.getSource()==tricepsCheckBox) {
+                    
+                } else if (e.getSource()==upperLegsCheckBox) {
+                    
+                } else if (e.getSource()==confirmButton) {
+
+                }
+            }
+        };
+        absCheck.addActionListener(listener);
+        backCheckBox.addActionListener(listener);
+        bicepsCheckBox.addActionListener(listener);
+        cardioCheckBox.addActionListener(listener);
+        chestCheckBox.addActionListener(listener);
+        glutesCheckBox.addActionListener(listener);
+        lowerLegsCheckBox.addActionListener(listener);
+        shouldersCheckBox.addActionListener(listener);
+        tricepsCheckBox.addActionListener(listener);
+        upperLegsCheckBox.addActionListener(listener);
+        confirmButton.addActionListener(listener);*/
     }
 }
